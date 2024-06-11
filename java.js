7fb318d999fdbdc87b54e5e3db33d6b8dd9e0da9
ismael-2306347 <163002396+ismael-2306347@users.noreply.github.com>
2024-06-11 11:43:58 -0300

    funcion java

--- java.js
+++ java.js
@@ -1,15 +1,61 @@
-function comprobarinputs(){
+/**
+ * La funcion sirve para comprobar que la fecha indicada sea valida y si esta es valida se lleva a la pestaña correspondiente
+ * @comprobarinputs
+ * preguntar a profe que iria bien aca :)
+ */
+function comprobarinputs() {
 
- const inputs = document.querySelectorAll('input');
- let Sonnumeros=true;
+    let inputDia = document.getElementById('dia');
+    let inputMes = document.getElementById('mes');
+    let inputAno = document.getElementById('año');
 
- //recorre inputs
-   inputs.forEach(input=>{
-       //verifica si son numeros
-       if(isNaN(input.value) || input.value.trim() === " "){
-             Sonnumeros= false;
-       }
 
-       })
+    let diavalido = !Number.isNaN(inputDia) || inputDia.value.trim() !== "";
+    let mesvalido = !Number.isNaN(inputMes) || inputMes.value.trim() !== "";
+    let anovalido = !Number.isNaN(inputAno) || inputAno.value.trim() !== "";
+
+
+    if (diavalido && mesvalido && anovalido) {
+        if (inputMes.value.trim() === 1) {
+            window.location.href = "Horoscopo segun el mes/Enero.html";
+        }
+        if (inputMes.value.trim() === 2) {
+            window.location.href = "Horoscopo segun el mes/Febrero.html";
+        }
+        if (inputMes.value.trim() === 3) {
+            window.location.href = "Horoscopo segun el mes/Marzo.html";
+        }
+        if (inputMes.value.trim() === 4) {
+            window.location.href = "Horoscopo segun el mes/Abril.html";
+        }
+        if (inputMes.value.trim() === 5) {
+            window.location.href = "Horoscopo segun el mes/Mayo.html";
+        }
+        if (inputMes.value.trim() === 6) {
+            window.location.href = "Horoscopo segun el mes/Junio.html";
+        }
+        if (inputMes.value.trim() === 7) {
+            window.location.href = "Horoscopo segun el mes/Julio.html";
+        }
+        if (inputMes.value.trim() === 8) {
+            window.location.href = "Horoscopo segun el mes/Agosto.html";
+        }
+        if (inputMes.value.trim() === 9) {
+            window.location.href = "Horoscopo segun el mes/Septiembre.html";
+        }
+        if (inputMes.value.trim() === 10) {
+            window.location.href = "Horoscopo segun el mes/Octubre.html";
+        }
+        if (inputMes.value.trim() === 11) {
+            window.location.href = "Horoscopo segun el mes/Noviembre.html";
+        }
+        if (inputMes.value.trim() === 12) {
+            window.location.href = "Horoscopo segun el mes/Diciembre.html";
+        }
+    } else {
+        window.location.href = "Error.html";
+    }
+
+
 }
     
