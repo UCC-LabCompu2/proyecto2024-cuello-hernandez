018c6db91512cd42612a0b0d9a075215d71e3e57
JuanHernandez-99 <juanhernandez0099@gmail.com>
2024-06-17 02:15:59 -0300

    Dibujo canvas

--- java.js
+++ java.js
@@ -9,48 +9,66 @@ function comprobarinputs() {
     const inputMes = document.getElementById('mes').value;
     const inputAno = document.getElementById('año').value;
 
-   if (isNaN(inputDia)|| isNaN(inputMes) || isNaN(inputAno) || inputDia<1 || inputDia>31|| inputMes<1|| inputMes>12 ||inputAno>2016|| inputAno<1924){
-       window.open("Error.html");
-   }else{
-       switch (inputMes){
-           case "1":
-               window.open("Horoscopo segun el mes/Enero.html");
-               break;
-           case "2":
-               window.open("Horoscopo segun el mes/Febrero.html");
-               break;
-           case "3":
-               window.open("Horoscopo segun el mes/Marzo.html");
-               break;
-           case "5":
-               window.open("Horoscopo segun el mes/Junio.html");
-               break;
-           case "6":
-               window.open("Horoscopo segun el mes/Junio.html");
-               break;
-           case "7":
-               window.open("Horoscopo segun el mes/Julio.html");
-               break;
-           case "8":
-               window.open("Horoscopo segun el mes/Agosto.html");
-               break;
-           case "9":
-               window.open("Horoscopo segun el mes/Septiembre.html");
-               break;
-           case "10":
-               window.open("Horoscopo segun el mes/Octubre.html");
-               break;
-           case "11":
-               window.open("Horoscopo segun el mes/Noviembre.html");
-               break;
-           case "12":
-               window.open("Horoscopo segun el mes/Diciembre.html");
+    if (isNaN(inputDia) || isNaN(inputMes) || isNaN(inputAno) || inputDia < 1 || inputDia > 31 || inputMes < 1 || inputMes > 12 || inputAno > 2016 || inputAno < 1924) {
+        window.open("Error.html");
+    } else {
+        switch (inputMes) {
+            case "1":
+                window.open("Horoscopo segun el mes/Enero.html");
+                break;
+            case "2":
+                window.open("Horoscopo segun el mes/Febrero.html");
+                break;
+            case "3":
+                window.open("Horoscopo segun el mes/Marzo.html");
+                break;
+            case "5":
+                window.open("Horoscopo segun el mes/Junio.html");
+                break;
+            case "6":
+                window.open("Horoscopo segun el mes/Junio.html");
+                break;
+            case "7":
+                window.open("Horoscopo segun el mes/Julio.html");
+                break;
+            case "8":
+                window.open("Horoscopo segun el mes/Agosto.html");
+                break;
+            case "9":
+                window.open("Horoscopo segun el mes/Septiembre.html");
+                break;
+            case "10":
+                window.open("Horoscopo segun el mes/Octubre.html");
+                break;
+            case "11":
+                window.open("Horoscopo segun el mes/Noviembre.html");
+                break;
+            case "12":
+                window.open("Horoscopo segun el mes/Diciembre.html");
 
-       }
+        }
 
 
-
-   }
+    }
 
 
 }
+
+/**
+ *La funcion sirve para dibujar un circulo violeta con canvas
+ * @dibujocanvas
+ */
+
+
+function dibujocanvas() {
+    const canvas = document.getElementById('Canvas');
+    const ctx = canvas.getContext('2d');
+    ctx.fillStyle = 'purple';
+    ctx.beginPath();
+    ctx.arc(100, 50, 50, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.closePath();
+}
+
+
+
